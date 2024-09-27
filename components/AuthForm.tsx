e3e668fa6cf5813c703b19004b669e392e762680
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-09-27 13:22:43 -0400

    revert running code

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -21,8 +21,11 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -38,17 +41,22 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  const onSubmit = async (
-    values: z.infer<typeof formSchema>
-  ): Promise<void> => {
+  const onSubmit = async (data: z.infer<typeof formSchema>): Promise<void> => {
     setIsLoading(true);
     try {
       // sign-up with app-write and create a plaid link token
 
       if (type === "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
 
       if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
       }
     } catch (error) {
       console.log(error);
@@ -112,6 +120,12 @@ const AuthForm = ({ type }: { type: string }) => {
                     name="address1"
                     label="Address"
                     placeholder="Enter your specific address"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your city"
                   />
                   <div className="flex gap-4">
                     <CustomInput
