8f8647d8c9b735f61cd93d9a01be9fab714a7bd8
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-09-28 13:27:06 -0400

    implement plaid and dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Image from "next/image";
 import { useState } from "react";
 
-import { set, z } from "zod";
+import { date, set, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
@@ -23,6 +23,7 @@ import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import PLaidLink from "./PLaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -47,7 +48,19 @@ const AuthForm = ({ type }: { type: string }) => {
       // sign-up with app-write and create a plaid link token
 
       if (type === "sign-up") {
-        const newUser = await signUp(data);
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password,
+        };
+        const newUser = await signUp(userData);
         setUser(newUser);
       }
 
@@ -92,7 +105,9 @@ const AuthForm = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/* Plaid Link */}</div>
+        <div className="flex flex-col gap-4">
+          <PLaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           {/* form starts here */}
